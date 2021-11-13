f91765e4fbda544ab56fff51e5b33053f0aa456f
Jack Cho <jack06215@gmail.com>
2021-11-14 00:24:34 +0800

    init

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:shop_app/constants.dart';
+import 'package:shop_app/models/Product.dart';
 import 'package:shop_app/screens/home/components/categories.dart';
+import 'package:shop_app/screens/home/components/item_card.dart';
 
 class Body extends StatelessWidget {
   const Body({Key? key}) : super(key: key);
@@ -19,6 +21,23 @@ class Body extends StatelessWidget {
                   ?.copyWith(fontWeight: FontWeight.bold)),
         ),
         const Categories(),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: kDefaultPaddin),
+            child: GridView.builder(
+                itemCount: products.length,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  mainAxisSpacing: kDefaultPaddin,
+                  crossAxisSpacing: kDefaultPaddin,
+                  childAspectRatio: 0.75,
+                ),
+                itemBuilder: (context, index) => ItemCard(
+                      product: products[index],
+                      press: () => {}
+                    )),
+          ),
+        ),
       ],
     );
   }
