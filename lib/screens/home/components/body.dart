d9e4abc981f4a7cdcaa14484506db3c196f05a22
Jack Cho <jack06215@gmail.com>
2021-11-13 23:51:44 +0800

    add text

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -3,10 +3,21 @@ import 'package:shop_app/constants.dart';
 
 class Body extends StatelessWidget {
   const Body({Key? key}) : super(key: key);
-  
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: kDefaultPaddin),
+          child: Text("Women",
+              style: Theme.of(context)
+                  .textTheme
+                  .headline5
+                  ?.copyWith(fontWeight: FontWeight.bold)),
+        ),
+      ],
+    );
   }
 }
