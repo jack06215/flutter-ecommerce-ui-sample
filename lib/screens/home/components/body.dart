b6bcf4cc64eca385e6c77ed3eb74d096db4188ef
Jack Cho <jack06215@gmail.com>
2021-11-14 00:12:14 +0800

    list view

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shop_app/constants.dart';
+import 'package:shop_app/screens/home/components/categories.dart';
 
 class Body extends StatelessWidget {
   const Body({Key? key}) : super(key: key);
@@ -17,6 +18,7 @@ class Body extends StatelessWidget {
                   .headline5
                   ?.copyWith(fontWeight: FontWeight.bold)),
         ),
+        const Categories(),
       ],
     );
   }
