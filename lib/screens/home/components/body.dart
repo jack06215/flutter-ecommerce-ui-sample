5ca7cc38a3f28f89e2aa08056b3a389515c92087
Jack Cho <jack06215@gmail.com>
2021-11-13 23:32:18 +0800

    body init

---
+++ lib/screens/home/components/body.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:shop_app/constants.dart';
+
+class Body extends StatelessWidget {
+  const Body({Key? key}) : super(key: key);
+  
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
